aa0094fe953189939d5aecb3c223488427ba1e4f
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-05-05 20:25:05 +0545

    responsive update to section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
+//  import WhatSetsUsApart from '../components/home/WhatSetsUsApart'
 
 function Home() {
   return (
     <div>
      <Hero/>
+     {/* <WhatSetsUsApart/> */}
     </div>
   )
 }
