369e30d68ac720ed4777fb49719c8ecad5303765
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-05-05 16:32:17 +0545

    chnages made for about

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Timeline from "../components/About/VerticalTimeline";
+import VerticalTimeline from "../components/About/VerticalTimeline";
+
+function About() {
+  return (
+    <div>
+      <VerticalTimeline />
+    </div>
+  );
+}
+
+export default About;
