31fefcb2eb341a38baa45971e4d96d7324d5c046
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-05-05 17:01:52 +0545

    change in hero section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Timeline from "../components/About/VerticalTimeline";
 import VerticalTimeline from "../components/About/VerticalTimeline";
+import AboutHero from "../components/About/AboutHero";
 
 function About() {
   return (
     <div>
+      <AboutHero />
       <VerticalTimeline />
     </div>
   );
