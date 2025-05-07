1ead83d60476613d9fc4af7a5ae467a6ee086d7b
bhaire7 <baibhavnepal07@gmail.com>
2025-05-07 13:45:54 +0545

    Yarn Section done

--- src/pages/Yarn.jsx
+++ src/pages/Yarn.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import NavBarSection from "../components/Yarn/NavBarSection";
+import Headhero from "../components/Yarn/Headhero";
+import YarnOfferings from "../components/Yarn/YarnOfferings";
 
 const Yarn = () => {
   return (
     <div>
       <NavBarSection />
+      <Headhero />
+      <YarnOfferings />
     </div>
   );
 };
