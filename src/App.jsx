ea6f341a54d7255ab9e0d0069b85e7910d274c2b
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-05-07 12:37:11 +0545

    our story compnent created

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import Home from "./pages/Home";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Yarn from "./pages/Yarn";
+import ScrollToTop from "./components/ScrollToTop";
+import AboutOurStory from "./components/About/AboutOurStory";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/about/our-story" element={<AboutOurStory />} />
           <Route path="*" element={<NoPage />} />
           {/* Fiber Page Routes */}
           <Route path="/fiber" element={<Fibers />} />
