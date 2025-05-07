037bf7661f35802707e42658a794117637266aa0
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-05-07 14:38:01 +0545

    leaflet map added

---
+++ src/components/map/MapContainerComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import MarkerComponent from './MarkerComponent';
+
+const MapContainerComponent = ({ countries, onMarkerClick }) => {
+  return (
+    <MapContainer
+      center={[20, 0]}  
+      zoom={2}    
+      scrollWheelZoom={false}    
+      style={{ width: '100%', height: '100%' }}
+    >
+    
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      
+      {countries.map((country, index) => (
+        <MarkerComponent
+          key={index}
+          country={country}
+          onClick={onMarkerClick}
+        />
+      ))}
+    </MapContainer>
+  );
+};
+
+export default MapContainerComponent;
