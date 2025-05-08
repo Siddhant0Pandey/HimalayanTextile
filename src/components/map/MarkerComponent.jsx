31745dcbfbc2058dc2554c3357433870648b5ad2
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-05-08 10:10:20 +0545

    map marker updated

--- src/components/map/MarkerComponent.jsx
+++ src/components/map/MarkerComponent.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 
+// Define marker icons for export and import
+const getIcon = (type) => {
+  const iconUrl =
+    type === 'export'
+      ? 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png'
+      : 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png';
+
+  return new L.Icon({
+    iconUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl:
+      'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    shadowSize: [41, 41],
+  });
+};
+
 const MarkerComponent = ({ country, onClick }) => {
   return (
     <Marker
@@ -9,11 +27,7 @@ const MarkerComponent = ({ country, onClick }) => {
       eventHandlers={{
         click: () => onClick(country.name),
       }}
-      icon={new L.Icon({
-        iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41], 
-      })}
+      icon={getIcon(country.type)}
     >
       <Popup>{country.name}</Popup>
     </Marker>
