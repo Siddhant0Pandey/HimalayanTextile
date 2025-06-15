4bb74d3d5d169d4be977b6ab856a2d47a412b10e
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-06-15 12:46:41 +0545

    our story animation update

--- src/components/About/OurStory/OurStoryFounderMsg.jsx
+++ src/components/About/OurStory/OurStoryFounderMsg.jsx
@@ -41,7 +41,7 @@ function OurStoryFounderMsg() {
   }, [isInView, controlsLeft, controlsRight]);
 
   return (
-    <motion.div className="py-12 md:py-20 max-w-[95%] md:max-w-[80%] m-auto">
+    <motion.div className="py-12 md:py-20 max-w-[100%] md:max-w-[100%] px-[5%] bg-white text-darkBg">
       {/* Title Animation */}
       <motion.h1
         className="text-2xl md:text-4xl text-center font-bold text-[#1FA951] mb-6 md:mb-12"
